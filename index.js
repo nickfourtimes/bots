179833a333c76ea8c14229ab4981bb23b2016475
Nick Rudzicz <nrudzicz@gmail.com>
2025-03-04 18:26:49 -0500

    Fixing vars

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { AtpAgent, RichText } = require("@atproto/api");
 async function sendPost(text) {
 	const agent = new AtpAgent({ service: "https://bsky.social" });
 	await agent.login({
-		identifier: "${{ env.PODRACING_IDENTIFIER }}",
-		password: "${{ secrets.PODRACING_PASSWORD }}",
+		identifier: process.env.BSKY_HANDLE,
+		password: process.env.BSKY_PASSWORD,
 	});
 	const richText = new RichText({ text });
 	await richText.detectFacets(agent);
