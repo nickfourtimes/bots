499596d6a7ddad0c1fcb707fdd5cfe96a371eecb
Nick Rudzicz <nrudzicz@gmail.com>
2025-03-10 07:56:09 -0400

    Got masto podracing working

--- not-podracing.py
+++ not-podracing.py
@@ -1,67 +1,57 @@
 import os
+import requests
+import sys
 
 from dotenv import load_dotenv
 from mastodon import Mastodon
 
+# get vars
 load_dotenv()
-
 ACCESS_TOKEN = os.getenv("PODRACING_MASTO_ACCESS_TOKEN")
-CLIENT_KEY = os.getenv("PODRACING_MASTO_CLIENT_KEY")
-CLIENT_SECRET = os.getenv("PODRACING_MASTO_CLIENT_SECRET")
-
-masto = Mastodon(access_token=ACCESS_TOKEN, api_base_url="http://mastodon.social")
-
-print(masto.toot("here goes nothing"))
+BASE_URL = os.getenv("MASTO_BASE_URL")
 
 
-# ************************************************************************* METHODS
+def get_post():
+    wordnik_req = (
+        "http://api.wordnik.com:80/v4/words.json/randomWord?"
+        + "hasDictionaryDef=false"
+        + "&includePartOfSpeech=noun"
+        + "&minCorpusCount=5000"
+        + "&maxCorpusCount=-1"
+        + "&minDictionaryCount=1"
+        + "&maxDictionaryCount=-1"
+        + "&minLength=4"
+        + "&maxLength=100"
+        + "&api_key="
+        + os.getenv("WORDNIK_API_KEY")
+    )
 
-# function doPost(res) {
-# 	var wordnik_req =
-# 		"http://api.wordnik.com:80/v4/words.json/randomWord?" +
-# 		"hasDictionaryDef=false" +
-# 		"&includePartOfSpeech=noun" +
-# 		"&minCorpusCount=5000" +
-# 		"&maxCorpusCount=-1" +
-# 		"&minDictionaryCount=1" +
-# 		"&maxDictionaryCount=-1" +
-# 		"&minLength=4" +
-# 		"&maxLength=100" +
-# 		"&api_key=" +
-# 		process.env.WORDNIK_API_KEY;
+    response = requests.get(wordnik_req)
 
-# 	var answer = "?";
-# 	request(wordnik_req, (error, response, body) => {
-# 		if (error || response.statusCode !== 200 || body == "") {
-# 			console.error("wordnik error: " + error);
-# 			res.send("null");
-# 		} else {
-# 			answer = JSON.parse(body);
+    try:
+        response = requests.get(wordnik_req)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
 
-# 			// get the correct article
-# 			var article = "A";
-# 			var first = answer.word.substr(0, 1);
-# 			if (
-# 				first === "a" ||
-# 				first === "e" ||
-# 				first === "i" ||
-# 				first === "o" ||
-# 				first === "u"
-# 			) {
-# 				article = "An";
-# 			}
+	# todo better error handling
+    if response.status_code != 200 or response.json == "":
+        print(f"wordnik error: {response.raise_for_status()}", file=sys.stderr)
+    else:
+        answer = response.json()["word"]
 
-# 			// *** THIS IS THE POST TEXT ***
-# 			var post_text = article + " " + answer.word + " is NOT podracing.";
+        # get the correct article
+        article = "A"
+        first = answer[0]
+        if first == "a" or first == "e" or first == "i" or first == "o" or first == "u":
+            article = "An"
 
-# 			// *** MASTODON ***
-# 			M.post("statuses", { status: post_text }, (err, data, response) => {
-# 				if (error) {
-# 					console.error(err);
-# 				}
-# 			});
+        # *** THIS IS THE POST TEXT ***
+        post_text = f"{article} {answer} is NOT podracing."
+        print(f"Post text: {post_text}")
+        return post_text
 
-# 			res.sendStatus(200);
-# 		} // end of "else all is well with the response from Wordnik"
-# 	});
-# }
+
+# poast!
+masto = Mastodon(access_token=ACCESS_TOKEN, api_base_url=BASE_URL)
+masto.status_post(get_post())
