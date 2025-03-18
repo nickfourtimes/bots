1595aee884a0d7f514f408d13e2fbc9b364e9944
Nick Rudzicz <nrudzicz@gmail.com>
2025-03-18 08:40:16 -0400

    Cleanup Use randint for random Lytton components

--- lytton/maps/map_gen.py
+++ lytton/maps/map_gen.py
@@ -1,23 +1,16 @@
-import math
 import random
 
 from PIL import Image
 
-IMG_PATH = "./lytton/maps/components/"
+# Constants
 
+IMG_PATH = "./lytton/maps/components/"
 MAP_WIDTH = 320
 MAP_HEIGHT = 168
-
 NUM_HIGHWAY_COMPS = 8
 LONG_BLOCK_CHANCE = 0.2
 
-
-def load_image(path):
-    print("hello")
-
-
-def rand_comp(maxNum):
-    return math.floor(random.random() * maxNum) + 1
+randint = random.randint
 
 
 def create_city_image_list(args):
@@ -29,18 +22,18 @@ def create_city_image_list(args):
 
     # draw a basic, random city
     # the other components, if selected, can just draw right on top of these
-    list.append(f"{IMG_PATH}TLL-0{rand_comp(4)}.png")
-    list.append(f"{IMG_PATH}TL-0{rand_comp(6)}.png")
-    list.append(f"{IMG_PATH}TR-0{rand_comp(7)}.png")
-    list.append(f"{IMG_PATH}TRR-0{rand_comp(5)}.png")
-    list.append(f"{IMG_PATH}LL-0{rand_comp(6)}.png")
-    list.append(f"{IMG_PATH}L-0{rand_comp(6)}.png")
-    list.append(f"{IMG_PATH}R-0{rand_comp(6)}.png")
-    list.append(f"{IMG_PATH}RR-0{rand_comp(7)}.png")
-    list.append(f"{IMG_PATH}BLL-0{rand_comp(6)}.png")
-    list.append(f"{IMG_PATH}BL-0{rand_comp(7)}.png")
-    list.append(f"{IMG_PATH}BR-0{rand_comp(8)}.png")
-    list.append(f"{IMG_PATH}BRR-0{rand_comp(7)}.png")
+    list.append(f"{IMG_PATH}TLL-0{randint(1, 4)}.png")
+    list.append(f"{IMG_PATH}TL-0{randint(1, 6)}.png")
+    list.append(f"{IMG_PATH}TR-0{randint(1, 7)}.png")
+    list.append(f"{IMG_PATH}TRR-0{randint(1, 5)}.png")
+    list.append(f"{IMG_PATH}LL-0{randint(1, 6)}.png")
+    list.append(f"{IMG_PATH}L-0{randint(1, 6)}.png")
+    list.append(f"{IMG_PATH}R-0{randint(1, 6)}.png")
+    list.append(f"{IMG_PATH}RR-0{randint(1, 7)}.png")
+    list.append(f"{IMG_PATH}BLL-0{randint(1, 6)}.png")
+    list.append(f"{IMG_PATH}BL-0{randint(1, 7)}.png")
+    list.append(f"{IMG_PATH}BR-0{randint(1, 8)}.png")
+    list.append(f"{IMG_PATH}BRR-0{randint(1, 7)}.png")
 
     # if there's a highway, it supercedes any other drawing
     if "highway" in args:
@@ -48,14 +41,14 @@ def create_city_image_list(args):
 
         # the little bits & bobs surrounding the highways
         for i in range(NUM_HIGHWAY_COMPS):
-            list.append(f"{IMG_PATH}Highway-01-0{rand_comp(3)}.png")
-            list.append(f"{IMG_PATH}Highway-02-0{rand_comp(3)}.png")
-            list.append(f"{IMG_PATH}Highway-03-0{rand_comp(4)}.png")
-            list.append(f"{IMG_PATH}Highway-04-0{rand_comp(4)}.png")
-            list.append(f"{IMG_PATH}Highway-05-0{rand_comp(3)}.png")
-            list.append(f"{IMG_PATH}Highway-06-0{rand_comp(4)}.png")
-            list.append(f"{IMG_PATH}Highway-07-0{rand_comp(3)}.png")
-            list.append(f"{IMG_PATH}Highway-08-0{rand_comp(3)}.png")
+            list.append(f"{IMG_PATH}Highway-01-0{randint(1, 3)}.png")
+            list.append(f"{IMG_PATH}Highway-02-0{randint(1, 3)}.png")
+            list.append(f"{IMG_PATH}Highway-03-0{randint(1, 4)}.png")
+            list.append(f"{IMG_PATH}Highway-04-0{randint(1, 4)}.png")
+            list.append(f"{IMG_PATH}Highway-05-0{randint(1, 3)}.png")
+            list.append(f"{IMG_PATH}Highway-06-0{randint(1, 4)}.png")
+            list.append(f"{IMG_PATH}Highway-07-0{randint(1, 3)}.png")
+            list.append(f"{IMG_PATH}Highway-08-0{randint(1, 3)}.png")
 
         # ...and then, we might have one of the highways that leads out of the city
         if "tr" in args["highway"]:
@@ -67,13 +60,13 @@ def create_city_image_list(args):
             if "tl" in args["cityLimit"]:
                 list.append(f"{IMG_PATH}Special-TL-CityLimit-01.png")
             elif "t" in args["cityLimit"]:
-                list.append(f"{IMG_PATH}Special-T-CityLimit-0{rand_comp(2)}.png")
+                list.append(f"{IMG_PATH}Special-T-CityLimit-0{randint(1, 2)}.png")
             elif "l" in args["cityLimit"]:
                 list.append(f"{IMG_PATH}Special-L-CityLimit-01.png")
             elif "r" in args["cityLimit"]:
-                list.append(f"{IMG_PATH}Special-R-CityLimit-0{rand_comp(2)}.png")
+                list.append(f"{IMG_PATH}Special-R-CityLimit-0{randint(1, 2)}.png")
             elif "b" in args["cityLimit"]:
-                list.append(f"{IMG_PATH}Special-B-CityLimit-0{rand_comp(2)}.png")
+                list.append(f"{IMG_PATH}Special-B-CityLimit-0{randint(1, 2)}.png")
         else:
             # some of the specials prevent us from drawing longer blocks on the sides
             canDoLongBlockL = True
@@ -110,9 +103,9 @@ def create_city_image_list(args):
 
             # random chance of drawing one of the long-blocks on the sides
             if canDoLongBlockL and random.random() < LONG_BLOCK_CHANCE:
-                list.append(f"{IMG_PATH}LL-Block-0{rand_comp(3)}.png")
+                list.append(f"{IMG_PATH}LL-Block-0{randint(1, 3)}.png")
             if canDoLongBlockR and random.random() < LONG_BLOCK_CHANCE:
-                list.append(f"{IMG_PATH}RR-Block-0{rand_comp(2)}.png")
+                list.append(f"{IMG_PATH}RR-Block-0{randint(1, 2)}.png")
 
     return list
 
